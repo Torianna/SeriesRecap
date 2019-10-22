44a81a4a909968133e5367569f57fb4a0f960b27
Klaudia <klaudia.goralska95@gmail.com>
2019-10-22 13:38:55 +0200

    Created repository for Series entity

---
+++ src/main/java/com/seriesrecap/SeriesRecap/Repositories/SeriesRepository.java
@@ -0,0 +1,9 @@
+package com.seriesrecap.SeriesRecap.Repositories;
+
+import com.seriesrecap.SeriesRecap.Entites.Series;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SeriesRepository extends JpaRepository <Series, Long> {
+}
