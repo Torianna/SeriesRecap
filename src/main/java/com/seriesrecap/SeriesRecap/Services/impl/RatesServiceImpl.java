03e843aa09dab460c0d43c0618d153433b6e099a
Klaudia <klaudia.goralska95@gmail.com>
2019-10-22 19:35:06 +0200

    Added implementation of RatesService

--- src/main/java/com/seriesrecap/SeriesRecap/Services/impl/RatesServiceImpl.java
+++ src/main/java/com/seriesrecap/SeriesRecap/Services/impl/RatesServiceImpl.java
@@ -1,4 +1,59 @@
 package com.seriesrecap.SeriesRecap.Services.impl;
 
-public class RatesServiceImpl {
+import com.seriesrecap.SeriesRecap.Entites.Rates;
+import com.seriesrecap.SeriesRecap.Repositories.RatesRepository;
+import com.seriesrecap.SeriesRecap.Services.RatesService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+
+public class RatesServiceImpl implements RatesService {
+
+    private final RatesRepository ratesRepository; //initialized by using lombok AllArgsConstructor
+
+    @Override
+    public Rates getRateById(Long id) {
+        Optional<Rates> rates = ratesRepository.findById(id);
+        return rates.orElse(null);
+
+    }
+
+    @Override
+    public Rates deleteRateById(Long id) {
+        Optional<Rates> rates= ratesRepository.findById(id);
+
+        if(rates.isPresent())
+        {
+            ratesRepository.deleteById(id);
+            return rates.get();
+        }
+        return null;
+    }
+
+    @Override
+    public Rates editRateById(Long id, Rates rate) {
+        Optional<Rates> rates= ratesRepository.findById(id);
+
+        if(rates.isPresent())
+        {
+            ratesRepository.deleteById(id);
+            ratesRepository.save(new Rates(rate.getId(),rate.getSeries(),
+                    rate.getPlot(),rate.getEffects(),rate.getBudget(),
+                    rate.getEnding());
+
+            return rate;
+        }
+        return null;
+    }
+
+    @Override
+    public Rates saveRate(Rates rate) {
+        return ratesRepository.save(rate);
+
+
+    }
 }
