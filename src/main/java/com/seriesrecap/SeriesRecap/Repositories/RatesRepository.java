36abf95e16bf33a98859c38ffcf0ad6019fc756c
Klaudia <klaudia.goralska95@gmail.com>
2020-08-23 17:24:05 +0200

    Clean code

--- src/main/java/com/seriesrecap/SeriesRecap/Repositories/RatesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.seriesrecap.SeriesRecap.Repositories;
-
-import com.seriesrecap.SeriesRecap.Entites.Rates;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RatesRepository extends JpaRepository <Rates, Integer> {
-}
--- src/main/java/com/seriesrecap/SeriesRecap/Repositories/TypesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.seriesrecap.SeriesRecap.Repositories;
-
-import com.seriesrecap.SeriesRecap.Entites.Types;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TypesRepository extends JpaRepository <Types, Integer> {
-}
